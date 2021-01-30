02d7973e05fab23d13a3735d3a89be3ddd9517e2
CoderOO7 <shubhamyadav.70007@gmail.com>
2021-01-30 23:36:10 +0530

    integrate openWeather api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("App loaded");
+import {
+  OPEN_WEATHER_API_BASE_URL,
+  OPEN_WEATHER_API_KEY,
+  OPEN_WEATHER_API_ENDPOINT_WEATHER,
+} from "./modules/constants.js";
+
+(function initApp(window, document) {
+  console.log("App loaded");
+
+  window.addEventListener("DOMContentLoaded", async (e) => {
+    let requestURL = `${OPEN_WEATHER_API_BASE_URL}/${OPEN_WEATHER_API_ENDPOINT_WEATHER}?q=delhi&appid=${OPEN_WEATHER_API_KEY}`;
+    let response = await fetch(requestURL);
+    let weatherData = await response.json();
+
+    console.log(weatherData);
+  });
+})(window, document);
---
+++ src/modules/constants.js
@@ -0,0 +1,6 @@
+export const OPEN_WEATHER_API_KEY = "7b36a65082c985be6a27657feb9c1629";
+export const OPEN_WEATHER_API_BASE_URL = "https://api.openweathermap.org";
+export const OPEN_WEATHER_API_ENDPOINT_WEATHER = "data/2.5/weather";
+export const API_TIMEOUT = 60000;
+export const API_ERROR_DEFAULT_MSG =
+  "Due to technical error unable to show weather data :(";
